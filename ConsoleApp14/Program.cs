d3b9781e4f6ca1a697d472f7b8e01f184544ea62
PC <PC@DEGHIE>
2023-03-07 21:05:21 +0800

    Add project files.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Odd numbers divisible by 5:");
+
+            for (int i = 5; i < 300; i += 10)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
